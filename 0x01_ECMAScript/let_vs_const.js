2b9dff141b13f943de40dad161f6e09556b19c9c
johnenes <johneneojo1@gmail.com>
2023-06-18 10:25:10 -0700

    let_vs_const.js

--- 0x01_ECMAScript/let_vs_const.js
+++ 0x01_ECMAScript/let_vs_const.js
@@ -5,9 +5,13 @@ var c = 5;
 let b;
 a = a+c;
 c = c+c;
-b = a-c;
+b
+    = a-c;
 
 console.log(a);
 console.log(b);
 console.log(a);
 
+//const usage on when variable can not be change
+const MAX_NUM = 100;
+const  pi = 3.145

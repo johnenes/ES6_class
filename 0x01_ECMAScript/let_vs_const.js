fb2d127290b64ddb2f508ac2f205dbf35892d91f
johnenes <johneneojo1@gmail.com>
2023-06-18 10:13:17 -0700

    let identifier vs const identifier

---
+++ 0x01_ECMAScript/let_vs_const.js
@@ -0,0 +1,13 @@
+//let is used when there is a need to resign values during the program
+//const is used with a fixed value when there is no need to add value during execution
+var a = 4;
+var c = 5;
+let b;
+a = a+c;
+c = c+c;
+b = a-c;
+
+console.log(a);
+console.log(b);
+console.log(a);
+
